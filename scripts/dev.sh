283e575ea8057222630bdc27c9f2ce6e239eb664
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2026-04-19 12:52:25 -0700

    feat: add stable-context-map slides and support any thumbnail extension

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 concurrently \
   "vite" \
-  "slidev slides/slidev-starter-1/slides.md --port 3030 --open false" \
-  "slidev slides/slides-test/slides.md --port 3031 --open false"
+  "slidev slides/stable-context-map/slides.md --port 3030 --open false" \
+  "slidev slides/slidev-starter-1/slides.md --port 3031 --open false" \
+  "slidev slides/slides-test/slides.md --port 3032 --open false"
